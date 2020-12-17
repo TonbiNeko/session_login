2d202d3cacb7f389da47a25098e3cfd20bf22984
Junko Mikami <junkomikami312nz@gmail.com>
2020-12-17 15:38:57 +0900

    add blog app and practice assosiative tables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -9,10 +10,12 @@ class UsersController < ApplicationController
     else
       render :new
     end
-    def show
-      @user = User.find(params[:id])
-    end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
